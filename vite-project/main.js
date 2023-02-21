421bfbd57947a4338edafd66114f9f72fc94588a
Adrian P. Mate <adrian.p.mate@gmail.com>
2023-02-21 16:01:18 +0100

    more adjustments and fine tuning to branch

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,19 +1,15 @@
 import "./style.scss";
 
-window.onload = function () {
-  window.addEventListener("scroll", function (e) {
-    if (window.pageYOffset > 100) {
-      document.querySelector("header").classList.add("is-scrolling");
-    } else {
-      document.querySelector("header").classList.remove("is-scrolling");
-    }
-  });
+const hamburgerMenu = document.querySelector(".hamburger");
 
-  const menu_btn = document.querySelector(".hamburger");
-  const mobile_menu = document.querySelector(".mobile-nav");
+const closeBtn = document.querySelector(".close-btn");
 
-  menu_btn.addEventListener("click", function () {
-    menu_btn.classList.toggle("is-active");
-    mobile_menu.classList.toggle("is-active");
-  });
-};
+const mobileNav = document.querySelector(".mobile-nav");
+
+hamburgerMenu.addEventListener("click", (e) => {
+  mobileNav.style.visibility = "visible";
+});
+
+closeBtn.addEventListener("click", (e) => {
+  mobileNav.style.visibility = "hidden";
+});
